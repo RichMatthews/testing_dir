925560c7f9807cacb4fc5fdbf407672e13a88c18
Rich Matthews <richmatthews@notonthehighstreet.com>
2017-01-11 10:33:47 +0000

    WIP

--- home/index.js
+++ home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import _ from 'underscore';
+import './index.scss';
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
@@ -16,12 +17,16 @@ class ReactApp extends React.Component{
       playersDeck: [],
       dealersDeck: [],
       resultOutcome: '',
-      revealCards: false
+      revealCards: false,
+      currentBalance: 1000,
+      stakeValue: 0,
+      submittedStakeValue: 0
     }
+    this.handleStakeChange = this.handleStakeChange.bind(this);
   }
 
   buildDeck = () => {
-    let suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+    let suit = ['C', 'D', 'H', 'S'];
     let rank = {
       'A': 1,
       '2': 2,
@@ -45,6 +50,13 @@ class ReactApp extends React.Component{
     });
     return deck;
   }
+
+  // componentDidMount = () => {
+  //   this.dealToPlayer();
+  //   this.dealToPlayer();
+  //   this.dealToDealer();
+  //   this.dealToDealer();
+  // }
 
   shuffleDeck = () => {
     return _.shuffle(this.buildDeck());
@@ -101,6 +113,7 @@ class ReactApp extends React.Component{
     this.setState({playersOverallTotal: total}, () => {
       this.dealerTwisted();
     });
+    this.revealCards();
   };
 
   handleClickTwist = () => {
@@ -123,20 +136,21 @@ class ReactApp extends React.Component{
   playerTwisted = (playersTotal) => {
     if(playersTotal > 21){
       this.setState({resultOutcome: 'dealer wins!'});
+      this.revealCards();
     }
-    this.revealCards();
   };
 
   revealCards = () => {
-    this.setState({revealCards: true})
+    this.setState({revealCards: true});
   }
 
   newGame = () => {
-    this.setState({dealersDeck: []})
-    this.setState({playersDeck: []})
-    this.setState({dealersOverallTotal: 0})
-    this.setState({playersOverallTotal: 0})
-    this.setState({resultOutcome: ''})
+    this.setState({dealersDeck: []});
+    this.setState({playersDeck: []});
+    this.setState({dealersOverallTotal: 0});
+    this.setState({playersOverallTotal: 0});
+    this.setState({resultOutcome: ''});
+    this.setState({revealCards: false});
   }
 
   dealerTwisted = () => {
@@ -144,6 +158,7 @@ class ReactApp extends React.Component{
     let playersTotal = this.state.playersOverallTotal;
     let looping = true;
     let outcome = '';
+    let moneyOutcome = '';
     while(looping){
       if(dealersTotal < 17){
         this.dealToDealer();
@@ -161,9 +176,11 @@ class ReactApp extends React.Component{
       else {
         if(dealersTotal > 21){
           outcome = 'player wins!';
+          moneyOutcome = this.state.stakeValue*2;
         }
         else if(playersTotal > dealersTotal){
           outcome = 'player wins!';
+          moneyOutcome = this.state.stakeValue*2;
         }
         else if (playersTotal == dealersTotal){
           outcome = 'tie!';
@@ -174,36 +191,96 @@ class ReactApp extends React.Component{
         looping = false;
       }
     }
+    let newBalance = this.state.currentBalance + moneyOutcome;
     this.setState({resultOutcome: outcome})
+    this.setState({currentBalance: newBalance})
   };
+
+  handleStakeChange(event) {
+    this.setState({stakeValue: event.target.value});
+  }
+
+  handleBetPlaced() {
+    let balance = this.state.currentBalance;
+    let stakeValue = this.state.stakeValue;
+    this.setState({currentBalance: balance - stakeValue})
+    this.setState({submittedStakeValue: stakeValue})
+    this.refs.stakeField.value = '';
+  }
 
   render(){
     return (
       <div>
-        Player:
+        Current Balance: £{this.state.currentBalance}
+        <br />
+        Place your stake: <label>£</label><input type="text" ref="stakeField" onChange={this.handleStakeChange}/>
+        <button onClick={this.handleBetPlaced.bind(this)}>Place Bet</button>
+        <br />
+        You have staked: £{this.state.submittedStakeValue}
+        <br />
+          <button onClick={this.handleClickStick.bind(this)}>Stick</button>
+          <button onClick={this.handleClickTwist.bind(this)}>Twist</button>
+          <button onClick={this.dealToPlayer.bind(this)}>Deal 2 Player</button>
+          <button onClick={this.dealToDealer.bind(this)}>Deal 2 Dealer</button>
+          <button onClick={this.newGame.bind(this)}>New Game</button>
+          <br />
+          Cards Left : {this.state.capturedDeck.length}
+          <br />
+          <span id="outcome"> {this.state.resultOutcome} </span>
+        <br />
+        <p className="participants" > Player: </p>
           <div>
           {this.state.playersDeck.map(function(card, index){
-            return <span key={ index }> {card.rankKey}{card.suit} </span>;
+              if (card.suit == 'D' || card.suit == 'H'){
+                return <div className="cardFormatDH" key={ index }> {card.rankKey}{card.suit} </div>;
+              }
+              else if (card.suit == 'C' || card.suit == 'S'){
+                return <div className="cardFormatCS" key={ index }> {card.rankKey}{card.suit} </div>;
+              }
           }, this)}
+          <span id="scores"> {this.state.playersOverallTotal} </span>
           </div>
-        Player Total: {this.state.playersOverallTotal}
-        <button onClick={this.handleClickStick.bind(this)}>Stick</button>
-        <button onClick={this.handleClickTwist.bind(this)}>Twist</button>
-        <button onClick={this.dealToPlayer.bind(this)}>Deal 2 Player</button>
-        <button onClick={this.dealToDealer.bind(this)}>Deal 2 Dealer</button>
-        <button onClick={this.newGame.bind(this)}>New Game</button>
         <br />
-        Dealer:
-          <br />
-            <div>
-            {this.state.dealersDeck.map(function(card, index){
-              return <span key={ index }> {card.rankKey}{card.suit} </span>;
-            }, this)}
-            </div>
-        Result: {this.state.resultOutcome}
-        <br />
-        Cards Left : {this.state.capturedDeck.length}
-      </div>
+          <div>
+            {this.state.revealCards
+              ?
+              <div>
+              <p className="participants" > Dealer: </p>
+              {this.state.dealersDeck.map(function(card, index){
+                if (index == 1){
+                  return <div className="cardFormatDH" key={ index }> {card.rankKey}{card.suit} </div>;
+                }
+                else{
+                  if (card.suit == 'D' || card.suit == 'H'){
+                    return <div className="cardFormatDH" key={ index }> {card.rankKey}{card.suit} </div>;
+                  }
+                  else if (card.suit == 'C' || card.suit == 'S'){
+                    return <div className="cardFormatCS" key={ index }> {card.rankKey}{card.suit} </div>;
+                  }
+                }
+              }, this)}
+              <span id="scores"> {this.state.dealersOverallTotal} </span>
+              </div>
+              :
+              <div>
+              <p className="participants" > Dealer: </p>
+              {this.state.dealersDeck.map(function(card, index){
+                if (index == 1){
+                  return <div className="cardFormatCS" key={ index }> {'-'}{'-'} </div>;
+                }
+                else{
+                  if (card.suit == 'D' || card.suit == 'H'){
+                    return <div className="cardFormatDH" key={ index }> {card.rankKey}{card.suit} </div>;
+                  }
+                  else if (card.suit == 'C' || card.suit == 'S'){
+                    return <div className="cardFormatCS" key={ index }> {card.rankKey}{card.suit} </div>;
+                  }
+                }
+              }, this)}
+              </div>
+            }
+        </div>
+  </div>
   )
  }
 };
